0fd7236bbbf9a9d2b8f20b1deb62129b5f11c331
hengchuan.qi <hengchuan.qi@semidrive.com>
2024-04-26 17:51:58 +0800

    update jlink gdb server config

--- gdb.sh
+++ gdb.sh
@@ -4,17 +4,17 @@
 file ./build/gd32vf103.elf
 
 
-set mem inaccessible-by-default off
+#set mem inaccessible-by-default off
 set arch riscv:rv32
 set remotetimeout 240
 
 ######################################
 # 连接到ocdGDBsever,请修改主机地址
 ######################################
-target extended-remote localhost:3333
+target remote localhost:2331
 
 monitor reset halt
-monitor flash protect 0 0 last off
+#monitor flash protect 0 0 last off
 
 load
 
