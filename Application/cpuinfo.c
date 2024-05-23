aa6742e28d30aba4d7e97d430bcc9eb4f0b5d29f
andeyqi <qihengchuan8888@163.com>
2024-05-23 21:43:00 +0800

    add misa read test code

---
+++ Application/cpuinfo.c
@@ -0,0 +1,55 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include "riscv_encoding.h"
+#include "n200_func.h"
+#include "littleshell.h"
+
+unsigned int cpuinfo(char argc,char ** argv)
+{
+	char str[20];
+	uint8_t offset = 0;
+	uint32_t misa;
+	misa = read_csr(misa);
+
+	if((misa&0xc0000000) == (0x01<<30))
+	{
+		str[0] = 'R';
+		str[1] = 'v';
+		str[2] = '3';
+		str[3] = '2';
+		offset += 4;
+	}
+	else if((misa&0xc0000000) == (0x02<<30))
+	{
+		str[0] = 'R';
+		str[1] = 'v';
+		str[2] = '6';
+		str[3] = '4';
+		offset += 4;
+	}
+	else
+	{
+		str[0] = 'R';
+		str[1] = 'v';
+		str[2] = '1';
+		str[3] = '2';
+		str[4] = '8';
+		offset += 5;
+	}
+
+	for(int i = 0; i < 26;i++)
+	{
+		if((0x01<<i)&misa)
+		{
+			str[offset] = 'A' + i;
+			offset++;
+		}
+	}
+
+	str[offset] = '\0';
+
+	printf("ISA : %s\n",str);
+    return 1;
+}
+LTSH_FUNCTION_EXPORT(cpuinfo,"show cpu info");
